ac916f747af3c71c894d0b78f3f182d7fad1198c
Pascal Sommer <p@pascalsommer.ch>
2021-12-19 23:42:20 +0100

    feat: test only one subtask in test_task!

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,28 @@ pub mod y2021;
 
 #[macro_export]
 macro_rules! test_task {
+    ($year:ident, $day:ident, $suffix:ident, ($sol_a:expr, _)) => {
+        paste::paste! {
+            #[test]
+            fn [<test_ $day _ $suffix>]() -> anyhow::Result<()> {
+                let path = format!("data/{}/{}_{}.txt", stringify!($year), stringify!($day), stringify!($suffix));
+                let sol = crate::$year::$day::solve(crate::io::file(path)?)?;
+                assert_eq!(sol.0, $sol_a);
+                Ok(())
+            }
+        }
+    };
+    ($year:ident, $day:ident, $suffix:ident, (_, $sol_b:expr)) => {
+        paste::paste! {
+            #[test]
+            fn [<test_ $day _ $suffix>]() -> anyhow::Result<()> {
+                let path = format!("data/{}/{}_{}.txt", stringify!($year), stringify!($day), stringify!($suffix));
+                let sol = crate::$year::$day::solve(crate::io::file(path)?)?;
+                assert_eq!(sol.1, $sol_b);
+                Ok(())
+            }
+        }
+    };
     ($year:ident, $day:ident, $suffix:ident, $solution:expr) => {
         paste::paste! {
             #[test]
